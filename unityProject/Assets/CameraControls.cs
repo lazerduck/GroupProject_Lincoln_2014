be30162dc087ac8d7d92a06201e55623555e816c
Adam Bowes <skylinegames@hotmail.co.uk>
2014-02-12 14:27:41 +0000

    Generate grid and some mouse control

---
+++ unityProject/Assets/CameraControls.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControls : MonoBehaviour {
+
+	public GameObject CameraObj;
+	public GUITexture CursorObj;
+	float MouseScroll = 0;
+	float MouseAccel = 0.01f;
+	void Start () {
+		Screen.lockCursor = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis("Mouse Y");
+		float v = Input.GetAxis("Mouse X");
+		Vector3 Pos = new Vector3(MouseAccel*v,MouseAccel*h,0);
+		MouseScroll = Input.GetAxis("Mouse ScrollWheel");
+		CameraObj.transform.Translate(0,MouseScroll,0);
+		CursorObj.transform.position += Pos;
+		while(CursorObj.transform.position.x < 25)
+		{
+			CursorObj.transform.position = new Vector3(0,CursorObj.transform.position.y);
+		}
+	}
+}
---
+++ unityProject/Assets/Create_map.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Create_map : MonoBehaviour {
+
+	public GameObject Tile;
+	public static int Rows = 5;
+	public static int Columns = 10;
+	public float TileSize = 1.5f;
+	public float ScrollAccel = 1.5f;
+	GameObject [] Tiles = new GameObject[Rows*Columns];
+
+	void Start () {
+		for(int i = 0; i<Rows;i++)
+		{
+			for(int j = 0; j<Columns;j++)
+			{
+				Vector3 Pos = new Vector3(j*TileSize,0,i*TileSize)*ScrollAccel;
+				GameObject TempTile = (GameObject)Instantiate(Tile);
+				TempTile.transform.Translate(Pos);
+				Tiles[i*j + j] = TempTile;
+			}
+		}
+	}
+
+
+	void Update () {
+	
+	}
+}
