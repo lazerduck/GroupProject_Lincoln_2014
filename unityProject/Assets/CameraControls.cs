aafb5339461fe284a214455dbdbfa0c9e01e78b0
Adam Bowes <skylinegames@hotmail.co.uk>
2014-02-12 23:34:07 +0000

    Added clicking on 3d world
    Any object witht the "Space" tag can be clicked and will move it 1 unit
    up. Took a while to get "veiwport position to world space translation"
    use as reference  for future

--- unityProject/Assets/CameraControls.cs
+++ unityProject/Assets/CameraControls.cs
@@ -2,18 +2,28 @@
 using System.Collections;
 
 public class CameraControls : MonoBehaviour {
-
+	//camera object
 	public GameObject CameraObj;
+	//mouse texture
 	public GUITexture CursorObj;
+	//mouse variables
 	float MouseScroll = 0;
 	float MouseAccel = 0.01f;
+	//width of the texture
 	float TexWidth = 0.03f;
+	//speed scrolling in x
 	float XSpeed = 0.3f;
+	//margin for scrolling
 	float Margin = 0.01f;
+	//columns imported from create map
 	public int Columns = 0;
+	//size of tiles
 	public float Size;
+	//distance the end of the map is from the center
 	float Spacer = 3;
+	//instance of the create map
 	Create_map Map;
+	Vector3 MouseClick;
 	void Start () {
 		Map = CameraObj.GetComponent<Create_map>();
 		Columns = Map.Columns;
@@ -22,6 +32,7 @@ public class CameraControls : MonoBehaviour {
 		Screen.lockCursor = true;
 		TexWidth = (float)CursorObj.texture.width/(float)Screen.width*2;
 		CameraObj.transform.Translate((float)(Columns/2)*(Size),0,0);
+		CursorObj.transform.position = new Vector3(CursorObj.transform.position.x,CursorObj.transform.position.y,5);
 	}
 	
 	// Update is called once per frame
@@ -34,9 +45,12 @@ public class CameraControls : MonoBehaviour {
 		//stor the mouse change as a vector3 and include mouse accelleration
 		Vector3 Pos = new Vector3(MouseAccel*v,MouseAccel*h*aspect,0);
 		//get the amount scrolled
-		MouseScroll = Input.GetAxis("Mouse ScrollWheel")*3;
+		MouseScroll = -Input.GetAxis("Mouse ScrollWheel")*3;
 		//mose the camera according to scrolling
-		CameraObj.transform.Translate(0,MouseScroll,0);
+		if(CameraObj.transform.position.y+MouseScroll > 1 && CameraObj.transform.position.y+MouseScroll < 10)
+		{
+			CameraObj.transform.Translate(0,MouseScroll,-MouseScroll);
+		}
 		//add the cange in mouse pos to the current cursor pos
 		CursorObj.transform.position += Pos;
 		//keep the mouse on the screen
@@ -76,6 +90,18 @@ public class CameraControls : MonoBehaviour {
 		{
 			
 			CursorObj.transform.position = new Vector3(CursorObj.transform.position.x,1+TexWidth);
+		}
+		//Clicking
+		if(Input.GetMouseButtonDown(0))
+		{
+			Vector3 mousepos = CursorObj.transform.position;
+			mousepos.x -= TexWidth;
+			RaycastHit hit = new RaycastHit();
+			Ray ray = Camera.main.ViewportPointToRay(mousepos);
+
+			if (Physics.Raycast (ray,out hit,100f)){
+				hit.transform.Translate(0,1,0);
+			}
 		}
 	}
 }
